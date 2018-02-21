651a31b534d5df4b6b089f5fd5bd1ad90ecfb781
Mei Kai Koh <meikaikoh@gmail.com>
2018-02-21 12:27:56 -0500

    Implemented toolbar

---
+++ README.md
@@ -0,0 +1,2 @@
+Paintbrush icon made by Freepik from www.flaticon.com 
+Select icon made by Pixel Perfect from www.flaticon.com
--- src/MainView.java
+++ src/MainView.java
@@ -13,10 +13,13 @@ public class MainView extends JFrame implements Observer {
      */
     public MainView(Model model) {
         // Set up the window.
-        this.setTitle("Your program title here!");
-        this.setMinimumSize(new Dimension(128, 128));
-        this.setSize(800, 600);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("CS 349 W18 A2");
+        setMinimumSize(new Dimension(128, 128));
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        createMenuBar();
+        createToolBar();
 
         // Hook up this observer so that it will be notified when the model
         // changes.
@@ -25,7 +28,6 @@ public class MainView extends JFrame implements Observer {
 
         setVisible(true);
     }
-
     /**
      * Update with data from the model.
      */
@@ -33,5 +35,43 @@ public class MainView extends JFrame implements Observer {
         // XXX Fill this in with the logic for updating the view when the model
         // changes.
         System.out.println("Model changed!");
+    }
+
+    private void createMenuBar() {
+        JMenuBar menubar=new JMenuBar();
+        JMenu file = new JMenu("File");
+        JMenu edit = new JMenu("Edit");
+        JMenu format = new JMenu("Format");
+        menubar.add(file);
+        menubar.add(edit);
+        menubar.add(format);
+        setJMenuBar(menubar);
+    }
+
+    private void createToolBar() {
+        JToolBar toolbar = new JToolBar();
+        toolbar.setFloatable(false);
+
+        ImageIcon cursor = new ImageIcon("src/icons/cursor.png");
+        ImageIcon paintBrush = new ImageIcon("src/icons/paintbrush.png");
+        JToggleButton cursorButton = new JToggleButton("Select", cursor, true);
+        JToggleButton paintBrushButton = new JToggleButton("Draw", paintBrush, false);
+
+        String[] drawingModeValues = { "Freeform line", "Straight line", "Rectangle", "Ellipse" };
+        JComboBox drawingModes = new JComboBox(drawingModeValues);
+
+        String[] strokeThickness = { "1px", "2px", "3px", "4px", "5px", "6px", "7px", "8px", "9px", "10px" };
+        JComboBox strokes = new JComboBox(strokeThickness);
+
+        JButton fillColor = new JButton("Fill Colour");
+        JButton strokeColor = new JButton("Stroke Colour");
+
+        toolbar.add(cursorButton);
+        toolbar.add(paintBrushButton);
+        toolbar.add(drawingModes);
+        toolbar.add(strokes);
+        toolbar.add(fillColor);
+        toolbar.add(strokeColor);
+        add(toolbar, BorderLayout.NORTH);
     }
 }
